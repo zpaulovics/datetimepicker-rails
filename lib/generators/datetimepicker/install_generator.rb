d53747a71f6c3a0331d3a5c7ca2e476e775b9867
zpaulovics <zpaulovics@gmail.com>
2013-05-23 10:01:52 +0200

    Correctied the installer module name

--- lib/generators/datetimepicker/install_generator.rb
+++ lib/generators/datetimepicker/install_generator.rb
@@ -1,6 +1,6 @@
 require 'securerandom'
 
-module Devise
+module Datetimepickers
   module Generators
     class InstallGenerator < Rails::Generators::Base
       source_root File.expand_path("../../templates/inputs", __FILE__)
