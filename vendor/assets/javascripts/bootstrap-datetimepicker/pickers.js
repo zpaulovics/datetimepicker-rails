568bde70ed4cfb30004d18655de58c79ce5ccf61
zpaulovics <zpaulovics@gmail.com>
2013-05-24 18:17:03 +0200

    Bump the version, commit the newly generated assets

---
+++ vendor/assets/javascripts/bootstrap-datetimepicker/pickers.js
@@ -0,0 +1,18 @@
+$(function() {
+    $('.datepicker').datetimepicker({
+        pickTime: false
+    });
+});
+
+$(function() {
+    $('.datetimepicker').datetimepicker({
+        pickSeconds: false
+    });
+});
+
+$(function() {
+    $('.timepicker').datetimepicker({
+        pickDate: false,
+        pickSeconds: false
+    });
+});
