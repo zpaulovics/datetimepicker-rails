52fdba1e4bb5874448d38e6944da7f4fc739557d
Dan Simpson <dan.simpson@gmail.com>
2013-10-10 11:14:23 -0700

    Disable time picker overlay for date_picker input

--- lib/generators/templates/inputs/date_picker_input.rb
+++ lib/generators/templates/inputs/date_picker_input.rb
@@ -10,7 +10,7 @@ class DatePickerInput < SimpleForm::Inputs::StringInput
     input_html_options[:data].merge!({ format: picker_pettern, language: I18n.locale.to_s,
                                        date_weekstart: I18n.t('datepicker.weekstart', :default => 0) })
 
-    template.content_tag :div, class: 'input-append date datetimepicker' do
+    template.content_tag :div, class: 'input-append date datepicker' do
       input = super # leave StringInput do the real rendering
       input += template.content_tag :span, class: 'add-on' do
         template.content_tag :i, '', class: 'icon-calendar', data: { 'time-icon' => 'icon-time', 'date-icon' => 'icon-calendar' }
