968c6fb94b6c36ab38fd68229a5cb04d6e246e8f
zpaulovics <zpaulovics@gmail.com>
2014-07-19 15:27:40 +0200

    Updgrade to Bootstrap 3 & Simpleform 3.1

--- lib/generators/templates/inputs/datetime_picker_input.rb
+++ lib/generators/templates/inputs/datetime_picker_input.rb
@@ -1,32 +1,28 @@
 class DatetimePickerInput < SimpleForm::Inputs::StringInput
-  def input
+  def input(wrapper_options)
     value = object.send(attribute_name) if object.respond_to? attribute_name
     display_pattern = I18n.t('datepicker.dformat', :default => '%d/%m/%Y') + ' ' + I18n.t('timepicker.dformat', :default => '%R')
     input_html_options[:value] ||= I18n.localize(value, :format => display_pattern) if value.present?
 
     input_html_options[:type] = 'text'
-    picker_pettern = I18n.t('datepicker.pformat', :default => 'dd/MM/yyyy') + ' ' + I18n.t('timepicker.pformat', :default => 'hh:mm')
+    picker_pettern = I18n.t('datepicker.pformat', :default => 'dd/MM/yyyy') + ' ' + I18n.t('timepicker.pformat', :default => 'HH:mm')
     input_html_options[:data] ||= {}
     input_html_options[:data].merge!({ format: picker_pettern, language: I18n.locale.to_s,
                                        date_weekstart: I18n.t('datepicker.weekstart', :default => 0) })
 
-    template.content_tag :div, class: 'input-append date datetimepicker' do
-      input = super # leave StringInput do the real rendering
+    template.content_tag :div, class: 'input-group date datetimepicker' do
+      input = super(wrapper_options) # leave StringInput do the real rendering
       input += template.content_tag :span, class: 'add-on' do
-        template.content_tag :i, '', class: 'icon-calendar', data: { 'time-icon' => 'icon-time', 'date-icon' => 'icon-calendar' }
+        template.content_tag :i, '', class: 'glyphicon glyphicon-calendar',
+                             data: { 'time-icon' => 'glyphicon glyphicon-time',
+                                     'date-icon' => 'glyphicon glyphicon-calendar' }
       end
       input
     end
   end
+
+  def input_html_classes
+    super.push ''   # 'form-control'
+  end
 end
 
-#class DatetimePickerInput < SimpleForm::Inputs::StringInput
-#  def input
-#    value = object.send(attribute_name) if object.respond_to? attribute_name
-#    # text_field_options['data-date-format'] = I18n.t('date.datepicker')
-#    input_html_options[:value] ||= I18n.localize(value, :format => '%Y/%m/%d %R') if value.present?
-#    input_html_classes << "datetimepicker"
-#
-#    super # leave StringInput do the real rendering
-#  end
-#end
