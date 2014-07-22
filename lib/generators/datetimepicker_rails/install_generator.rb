5d88b88684084f8c78e06fe79c0360b5a750a648
zpaulovics <zpaulovics@gmail.com>
2014-07-22 21:17:42 +0200

    Upgraded to Rails 4+ & Bootstrap 3+ using Eonasdan's repo.

--- lib/generators/datetimepicker_rails/install_generator.rb
+++ lib/generators/datetimepicker_rails/install_generator.rb
@@ -3,24 +3,61 @@ require 'securerandom'
 module DatetimepickerRails
   module Generators
     class InstallGenerator < Rails::Generators::Base
-      source_root File.expand_path("../../templates/inputs", __FILE__)
+      source_root File.expand_path("../../templates", __FILE__)
+
+      def source_path
+        File.expand_path(File.dirname(__FILE__))
+      end
 
       desc "Creates the Simple form custom fields definition for date, datetime and time pickers."
 
       def copy_datetime_picker
-        template "datetime_picker_input.rb", "app/inputs/datetime_picker_input.rb" unless
+        template "inputs/datetime_picker_input.rb", "app/inputs/datetime_picker_input.rb" unless
             File.exist?('app/inputs/datetime_picker_input.rb')
       end
 
       def copy_date_picker
-        template "date_picker_input.rb", "app/inputs/date_picker_input.rb" unless
+        template "inputs/date_picker_input.rb", "app/inputs/date_picker_input.rb" unless
             File.exist?('app/inputs/date_picker_input.rb')
       end
 
       def copy_time_picker
-        template "time_picker_input.rb", "app/inputs/time_picker_input.rb" unless
+        template "inputs/time_picker_input.rb", "app/inputs/time_picker_input.rb" unless
             File.exist?('app/inputs/time_picker_input.rb')
       end
+
+      def copy_pickers_js
+        copy_file("#{source_paths.last}/js/pickers.js",
+                  "vendor/assets/javascripts/pickers.js")
+      end
+
+      # Source the submodule with git://github.com/eonasdan/bootstrap-datetimepicker.git
+      def copy_bootstrap_datetimepicker
+
+        # update the submodule to the latest source version
+        git submodule: 'init'
+        git submodule: 'update'
+
+        repo_root = File.join(File.expand_path("../../..", File.dirname(__FILE__)),
+                              "bootstrap-datetimepicker")
+
+        copy_file("#{repo_root}/build/css/bootstrap-datetimepicker.min.css",
+                  "vendor/assets/stylesheets/bootstrap-datetimepicker.min.css")
+
+        copy_file("#{repo_root}/build/css/bootstrap-datetimepicker.css",
+                  "vendor/assets/stylesheets/bootstrap-datetimepicker.css")
+
+        app_root = File.expand_path(File.dirname("."))
+        inside("#{repo_root}/src/js") do
+          file_paths = Dir["**/**/*"].select { |f| File.file?(f) }
+
+          file_paths.each do |file_path|
+            copy_file("#{repo_root}/src/js/#{file_path}", "#{app_root}/vendor/assets/javascripts//#{file_path}")
+          end
+        end
+
+      end
+
     end
   end
 end
